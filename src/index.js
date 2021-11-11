c4b45025c3b0e969f123145a2278c21eaf64b1ac
Dyonatha <dkramer97@hotmail.com>
2021-11-11 14:51:51 -0300

    Exercícios iniciais

--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
+const { response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.send('Oi');
+const customers = [];
+
+app.post('/account', (request, response) => {
+    const { cpf, name } = request.body;
+
+    customerCpfAlreadyInUse = customers.some((customer) => customer.cpf === cpf);
+    if (customerCpfAlreadyInUse) {
+        return response.status(400).send('This cpf is already in use');
+    }
+
+    customers.push(
+        {
+            name,
+            cpf,
+            id: uuidv4(),
+            statement: []
+        }
+    );
+
+    return response.status(201).send();
 });
+
+app.get('/statement', (request, response) => {
+    const { cpf } = request.headers;
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: 'Customer not found' });
+    }
+
+    return response.json(customer.statement);
+})
 
 app.listen(3333);
