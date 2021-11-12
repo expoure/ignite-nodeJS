5c77bb437a77015df4ca6bb043ec5bd8a29cade5
Dyonatha <dkramer97@hotmail.com>
2021-11-12 09:08:45 -0300

    Exercícios iniciais

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
@@ -6,6 +5,30 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
+
+function verifyIfExistAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if (!customer) {
+        return response.status(400).json({error: 'Customer not find' });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
+function getBalance(statement) {
+    return statement.reduce((acc, operation) => {
+        if (operation.type === 'CREDIT') {
+            return acc + operation.amount;
+        }
+        else {
+            return acc - operation.amount;
+        }
+    }, 0);
+}
 
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body;
@@ -27,15 +50,30 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-    const { cpf } = request.headers;
-    const customer = customers.find((customer) => customer.cpf === cpf);
+app.get('/statement', verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
+})
 
-    if (!customer) {
-        return response.status(400).json({ error: 'Customer not found' });
+app.post('/deposit', verifyIfExistAccountCPF, (request, response) => {
+    const { amount, description } = request.body;
+    const { customer } = request; 
+    customer.statement.push({ amount, description, created_at: new Date(), type: 'CREDIT'});
+
+    return response.status(201).send();
+})
+
+app.post('/withdrawal', verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request; 
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({error: 'There is not money enough' });
     }
 
-    return response.json(customer.statement);
+    customer.statement.push({ amount, created_at: new Date(), type: 'DEBIT'});
+    return response.status(201).send();
 })
 
 app.listen(3333);
